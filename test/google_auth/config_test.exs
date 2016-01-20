fb32e10df3823fed0a80ea5fed10ce37cf419063
Phil Burrows <github@philburrows.com>
2016-01-19 23:39:29 -0600

    make sure all keys get converted to strings in the config

--- test/google_auth/config_test.exs
+++ test/google_auth/config_test.exs
@@ -2,9 +2,19 @@ defmodule GoogleAuth.ConfigTest do
   use ExUnit.Case
   alias GoogleAuth.Config
 
-  test "setting and retrieving values" do
+  test "setting and retrieving value" do
     Config.set(:key, "123")
     assert {:ok, "123"} == Config.get(:key)
+  end
+
+  test "setting a value by atom can be retrieved by string" do
+    Config.set(:random, "value")
+    assert {:ok, "value"} == Config.get("random")
+  end
+
+  test "setting a value by string can be retrieved by atom" do
+    Config.set("totally", "cool")
+    assert {:ok, "cool"} == Config.get(:totally)
   end
 
   test "the initial state is what's passed in from the app config" do
