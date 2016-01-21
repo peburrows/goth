45d2d0d1fca8879d8bbed8b47d702e6c757c7d73
Phil Burrows <github@philburrows.com>
2016-01-20 23:50:53 -0600

    docs!

--- lib/goth/token.ex
+++ lib/goth/token.ex
@@ -1,8 +1,33 @@
 defmodule Goth.Token do
+  @moduledoc """
+  Interface for retrieving access tokens, from either the `Goth.TokenStore`
+  or the Google token API. The first request for a token will hit the API,
+  but subsequent requests will retrieve the token from Goth's token store
+  """
+
   alias Goth.TokenStore
   alias Goth.Client
 
   defstruct [:token, :type, :expires]
+
+  @doc """
+  Get a `%Goth.Token{}` for a particular `scope`. `scope` can be a single
+  scope or multiple scopes joined by a comma.
+
+  ## Example
+  iex> Token.for_scope("https://www.googleapis.com/auth/pubsub")
+  {:ok, %Goth.Token{expires: ..., token: "...", type: "..."} }
+  """
+  def for_scope(scope) do
+    case TokenStore.find(scope) do
+      :error       -> retrieve_and_store!(scope)
+      {:ok, token} -> {:ok, token}
+    end
+  end
+
+  @doc """
+  Parse a successful JSON response from Google's token API and extract a `%Goth.Token{}`
+  """
 
   def from_response_json(json) do
     {:ok, attrs} = json |> Poison.decode
@@ -11,13 +36,6 @@ defmodule Goth.Token do
       type:    attrs["token_type"],
       expires: :os.system_time(:seconds) + attrs["expires_in"]
     }
-  end
-
-  def for_scope(scope) do
-    case TokenStore.find(scope) do
-      :error       -> retrieve_and_store!(scope)
-      {:ok, token} -> {:ok, token}
-    end
   end
 
   defp retrieve_and_store!(scope) do
--- mix.exs
+++ mix.exs
@@ -36,7 +36,9 @@ defmodule Goth.Mixfile do
      {:httpoison, "~> 0.8.0"},
      {:poison, "~> 1.5.2"},
      {:bypass, "~> 0.1", only: :test},
-     {:mix_test_watch, "~> 0.2.5", only: :dev}]
+     {:mix_test_watch, "~> 0.2.5", only: :dev},
+     {:ex_doc, "~> 0.11.3", only: [:dev]},
+     {:earmark, "~> 0.2", only: [:dev]}]
   end
 
   defp description do
