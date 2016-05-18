674285f7bcf2a16e00224b9a9b530ab8c34113ac
Phil Burrows <github@philburrows.com>
2016-05-18 14:54:51 -0500

    version bump 0.1.2 --> 0.1.3

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Goth.Mixfile do
 
   def project do
     [app: :goth,
-     version: "0.1.2",
+     version: "0.1.3",
      description: description,
      package: package,
      elixir: "~> 1.2",
