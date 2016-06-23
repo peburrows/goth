4eea521014518c217028959d4d02a0070818ada6
Phil Burrows <github@philburrows.com>
2016-06-23 10:58:36 -0400

    remove some comments that aren't needed

--- mix.exs
+++ mix.exs
@@ -10,9 +10,6 @@ defmodule Goth.Mixfile do
      deps: deps]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
     [
       mod: {Goth, []},
@@ -20,15 +17,6 @@ defmodule Goth.Mixfile do
     ]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     [{:json_web_token, "~> 0.2.5"},
      {:httpoison, "~> 0.8.0"},
