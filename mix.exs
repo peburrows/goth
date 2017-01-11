648374bda3a4a31607a4e31278f0eed84c4f47ee
Phil Burrows <github@philburrows.com>
2017-01-11 18:14:56 -0500

    update the version of httpoison we depend on

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Goth.Mixfile do
 
   def project do
     [app: :goth,
-     version: "0.2.1",
+     version: "0.3.0",
      description: description,
      package: package,
      elixir: "~> 1.2",
@@ -19,13 +19,13 @@ defmodule Goth.Mixfile do
 
   defp deps do
     [{:json_web_token, "~> 0.2.5"},
-     {:httpoison, "~> 0.9.0"},
+     {:httpoison, "~> 0.11"},
      {:poison, "~> 2.1"},
-     {:bypass, "~> 0.1", only: :test},
+     {:bypass, "~> 0.1",         only: :test},
      {:mix_test_watch, "~> 0.2", only: :dev},
-     {:ex_doc, "~> 0.11.3", only: [:dev]},
-     {:earmark, "~> 0.2", only: [:dev]},
-     {:credo, "~> 0.4", only: [:test, :dev]}
+     {:ex_doc, "~> 0.11.3",      only: :dev},
+     {:earmark, "~> 0.2",        only: :dev},
+     {:credo, "~> 0.4",          only: [:test, :dev]}
     ]
   end
 
