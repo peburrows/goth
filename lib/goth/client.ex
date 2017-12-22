4dbede58bc4abddaa0217549ff17a7daaf5c6ac0
Chengyin Liu <chengyin@slab.com>
2017-12-21 18:11:44 -0800

    Add sub support
    The current APIs are all perserved
    The support for config `:actor_email` is also perserved, which can be overriden

--- lib/goth/client.ex
+++ lib/goth/client.ex
@@ -18,14 +18,16 @@ defmodule Goth.Client do
   retrieve a new token.
   """
 
-  def get_access_token(scope) do
+  def get_access_token(scope), do: get_access_token(scope, [])
+  def get_access_token(scope, opts) when is_binary(scope) and is_list(opts) do
     {:ok, token_source} = Config.get(:token_source)
-    get_access_token(token_source, scope)
+    get_access_token(token_source, scope, opts)
   end
 
+  def get_access_token(source, scope, opts \\ [])
   # Fetch an access token from Google's metadata service for applications running
   # on Google's Cloud platform.
-  def get_access_token(:metadata, scope) do
+  def get_access_token(:metadata, scope, _opts) do
     headers  = [{"Metadata-Flavor", "Google"}]
     account  = Application.get_env(:goth, :metadata_account, "default")
     metadata = Application.get_env(:goth, :metadata_url,
@@ -39,23 +41,24 @@ defmodule Goth.Client do
   end
 
   # Fetch an access token from Google's OAuth service using a JWT
-  def get_access_token(:oauth_jwt, scope) do
+  def get_access_token(:oauth_jwt, scope, opts) do
+    %{sub: sub} = destruct_opts(opts)
     endpoint = Application.get_env(:goth, :endpoint, "https://www.googleapis.com")
     url      = "#{endpoint}/oauth2/v4/token"
     body     = {:form, [grant_type: "urn:ietf:params:oauth:grant-type:jwt-bearer",
-                        assertion:  jwt(scope)]}
+                        assertion:  jwt(scope, opts)]}
     headers  = [{"Content-Type", "application/x-www-form-urlencoded"}]
 
     {:ok, response} = HTTPoison.post(url, body, headers)
     if response.status_code >= 200 && response.status_code < 300 do
-      {:ok, Token.from_response_json(scope, response.body)}
+      {:ok, Token.from_response_json(scope, sub, response.body)}
     else
       {:error, "Could not retrieve token, response: #{response.body}"}
     end
   end
 
   # Fetch an access token from Google's OAuth service using a refresh token
-  def get_access_token(:oauth_refresh, scope) do
+  def get_access_token(:oauth_refresh, scope, _opts) do
     {:ok, refresh_token} = Config.get(:refresh_token)
     {:ok, client_id} = Config.get(:client_id)
     {:ok, client_secret} = Config.get(:client_secret)
@@ -75,8 +78,10 @@ defmodule Goth.Client do
     end
   end
 
-  def claims(scope), do: claims(scope, :os.system_time(:seconds))
-  def claims(scope, iat) do
+  def claims(scope, opts \\ [])
+  def claims(scope, iat) when is_integer(iat), do: claims(scope, iat: iat)
+  def claims(scope, opts) when is_list(opts) do
+    %{iat: iat, sub: sub} = destruct_opts(opts)
     {:ok, email} = Config.get(:client_email)
     c =
       %{
@@ -86,24 +91,28 @@ defmodule Goth.Client do
         "iat"   => iat,
         "exp"   => iat+10
       }
-    case Config.get(:actor_email) do
-      {:ok, sub} -> Map.put(c, "sub", sub)
-      _ -> c
+
+    if sub do
+      Map.put(c, "sub", sub)
+    else
+      c
     end
   end
 
-  def json(scope), do: json(scope, :os.system_time(:seconds))
-  def json(scope, iat) do
+  def json(scope, opts \\ [])
+  def json(scope, iat) when is_integer(iat), do: json(scope, iat: iat)
+  def json(scope, opts) when is_list(opts) do
     scope
-    |> claims(iat)
+    |> claims(opts)
     |> Poison.encode!
   end
 
-  def jwt(scope), do: jwt(scope, :os.system_time(:seconds))
-  def jwt(scope, iat) do
+  def jwt(scope, opts \\ []) 
+  def jwt(scope, iat) when is_integer(iat), do: jwt(scope, iat: iat)
+  def jwt(scope, opts) when is_list(opts) do
     {:ok, key} = Config.get(:private_key)
     scope
-    |> claims(iat)
+    |> claims(opts)
     |> JsonWebToken.sign(%{alg: "RS256", key: JsonWebToken.Algorithm.RsaUtil.private_key(key)})
   end
 
@@ -115,5 +124,19 @@ defmodule Goth.Client do
       "http://metadata.google.internal")
     url      = "#{metadata}/#{endpoint}"
     HTTPoison.get!(url, headers).body
+  end
+  
+  defp destruct_opts(opts) do
+    defaults = [
+      iat: :os.system_time(:seconds),
+      sub: case Config.get(:actor_email) do
+        {:ok, sub} -> sub
+        _ -> nil
+      end
+    ]
+
+    defaults
+    |> Keyword.merge(opts)
+    |> Enum.into(%{})
   end
 end
