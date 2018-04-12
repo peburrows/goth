b676f246efc7d41335204c7893635f8572148764
Phil Burrows <github@philburrows.com>
2018-04-11 20:24:40 -0500

    when an HTTP call get_access_token/2 fails hard, return {:error, _}

--- lib/goth/client.ex
+++ lib/goth/client.ex
@@ -32,6 +32,7 @@ defmodule Goth.Client do
   """
 
   def get_access_token(scope), do: get_access_token(scope, [])
+
   def get_access_token(scope, opts) when is_binary(scope) and is_list(opts) do
     {:ok, token_source} = Config.get(:token_source)
     get_access_token(token_source, scope, opts)
@@ -42,14 +43,13 @@ defmodule Goth.Client do
   # Fetch an access token from Google's metadata service for applications running
   # on Google's Cloud platform.
   def get_access_token(:metadata, scope, _opts) do
-    headers  = [{"Metadata-Flavor", "Google"}]
-    account  = Application.get_env(:goth, :metadata_account, "default")
-    metadata = Application.get_env(:goth, :metadata_url,
-                                   "http://metadata.google.internal")
+    headers = [{"Metadata-Flavor", "Google"}]
+    account = Application.get_env(:goth, :metadata_account, "default")
+    metadata = Application.get_env(:goth, :metadata_url, "http://metadata.google.internal")
     endpoint = "computeMetadata/v1/instance/service-accounts"
     url_base = "#{metadata}/#{endpoint}/#{account}"
 
-    url      = "#{url_base}/token"
+    url = "#{url_base}/token"
     {:ok, token} = HTTPoison.get(url, headers)
     {:ok, Token.from_response_json(scope, token.body)}
   end
@@ -58,17 +58,16 @@ defmodule Goth.Client do
   def get_access_token(:oauth_jwt, scope, opts) do
     %{sub: sub} = destruct_opts(opts)
     endpoint = Application.get_env(:goth, :endpoint, "https://www.googleapis.com")
-    url      = "#{endpoint}/oauth2/v4/token"
-    body     = {:form, [grant_type: "urn:ietf:params:oauth:grant-type:jwt-bearer",
-                        assertion:  jwt(scope, opts)]}
-    headers  = [{"Content-Type", "application/x-www-form-urlencoded"}]
+    url = "#{endpoint}/oauth2/v4/token"
 
-    {:ok, response} = HTTPoison.post(url, body, headers)
-    if response.status_code >= 200 && response.status_code < 300 do
-      {:ok, Token.from_response_json(scope, sub, response.body)}
-    else
-      {:error, "Could not retrieve token, response: #{response.body}"}
-    end
+    body =
+      {:form,
+       [grant_type: "urn:ietf:params:oauth:grant-type:jwt-bearer", assertion: jwt(scope, opts)]}
+
+    headers = [{"Content-Type", "application/x-www-form-urlencoded"}]
+
+    HTTPoison.post(url, body, headers)
+    |> handle_response(scope)
   end
 
   # Fetch an access token from Google's OAuth service using a refresh token
@@ -77,34 +76,37 @@ defmodule Goth.Client do
     {:ok, client_id} = Config.get(:client_id)
     {:ok, client_secret} = Config.get(:client_secret)
     endpoint = Application.get_env(:goth, :endpoint, "https://www.googleapis.com")
-    url      = "#{endpoint}/oauth2/v4/token"
-    body     = {:form, [grant_type: "refresh_token",
-                        refresh_token: refresh_token,
-                        client_id: client_id,
-                        client_secret: client_secret]}
-    headers  = [{"Content-Type", "application/x-www-form-urlencoded"}]
+    url = "#{endpoint}/oauth2/v4/token"
 
-    {:ok, response} = HTTPoison.post(url, body, headers)
-    if response.status_code >= 200 && response.status_code < 300 do
-      {:ok, Token.from_response_json(scope, response.body)}
-    else
-      {:error, "Could not retrieve token, response: #{response.body}"}
-    end
+    body =
+      {:form,
+       [
+         grant_type: "refresh_token",
+         refresh_token: refresh_token,
+         client_id: client_id,
+         client_secret: client_secret
+       ]}
+
+    headers = [{"Content-Type", "application/x-www-form-urlencoded"}]
+
+    HTTPoison.post(url, body, headers)
+    |> handle_response(scope)
   end
 
   def claims(scope, opts \\ [])
   def claims(scope, iat) when is_integer(iat), do: claims(scope, iat: iat)
+
   def claims(scope, opts) when is_list(opts) do
     %{iat: iat, sub: sub} = destruct_opts(opts)
     {:ok, email} = Config.get(:client_email)
-    c =
-      %{
-        "iss"   => email,
-        "scope" => scope,
-        "aud"   => "https://www.googleapis.com/oauth2/v4/token",
-        "iat"   => iat,
-        "exp"   => iat+10
-      }
+
+    c = %{
+      "iss" => email,
+      "scope" => scope,
+      "aud" => "https://www.googleapis.com/oauth2/v4/token",
+      "iat" => iat,
+      "exp" => iat + 10
+    }
 
     if sub do
       Map.put(c, "sub", sub)
@@ -115,16 +117,19 @@ defmodule Goth.Client do
 
   def json(scope, opts \\ [])
   def json(scope, iat) when is_integer(iat), do: json(scope, iat: iat)
+
   def json(scope, opts) when is_list(opts) do
     scope
     |> claims(opts)
-    |> Poison.encode!
+    |> Poison.encode!()
   end
 
-  def jwt(scope, opts \\ []) 
+  def jwt(scope, opts \\ [])
   def jwt(scope, iat) when is_integer(iat), do: jwt(scope, iat: iat)
+
   def jwt(scope, opts) when is_list(opts) do
     {:ok, key} = Config.get(:private_key)
+
     scope
     |> claims(opts)
     |> JsonWebToken.sign(%{alg: "RS256", key: JsonWebToken.Algorithm.RsaUtil.private_key(key)})
@@ -132,25 +137,33 @@ defmodule Goth.Client do
 
   @doc "Retrieves the project ID from Google's metadata service"
   def retrieve_metadata_project do
-    headers  = [{"Metadata-Flavor", "Google"}]
+    headers = [{"Metadata-Flavor", "Google"}]
     endpoint = "computeMetadata/v1/project/project-id"
-    metadata = Application.get_env(:goth, :metadata_url,
-      "http://metadata.google.internal")
-    url      = "#{metadata}/#{endpoint}"
+    metadata = Application.get_env(:goth, :metadata_url, "http://metadata.google.internal")
+    url = "#{metadata}/#{endpoint}"
     HTTPoison.get!(url, headers).body
   end
-  
+
   defp destruct_opts(opts) do
     defaults = [
       iat: :os.system_time(:seconds),
-      sub: case Config.get(:actor_email) do
-        {:ok, sub} -> sub
-        _ -> nil
-      end
+      sub:
+        case Config.get(:actor_email) do
+          {:ok, sub} -> sub
+          _ -> nil
+        end
     ]
 
     defaults
     |> Keyword.merge(opts)
     |> Enum.into(%{})
   end
+
+  defp handle_response({:ok, %{body: body, status_code: code}}, scope) when code in 200..299,
+    do: {:ok, Token.from_response_json(scope, body)}
+
+  defp handle_response({:ok, %{body: body}}, _scope),
+    do: {:error, "Could not retrieve token, response: #{body}"}
+
+  defp handle_response(other, _scope), do: other
 end
