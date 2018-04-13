a47f3aea1e4f340c40c22de33a1495afc2d328fc
Antoine POPINEAU <antoine.popineau@appscho.com>
2018-04-13 10:14:39 +0200

    Allow for ingesting multiple SAs from one JSON file. Allow for selecting which SA to use when requesting or refreshing a token.

--- lib/goth/token.ex
+++ lib/goth/token.ex
@@ -17,7 +17,19 @@ defmodule Goth.Token do
       {:ok, %Goth.Token{token: "23984723",
                         type: "Bearer",
                         scope: "https://www.googleapis.com/auth/pubsub",
-                        expires: 1453653825}}
+                        expires: 1453653825,
+                        account: :default}}
+
+  If the passed credentials contain multiple service account, you can change
+  the first parametter to be {client_email, scopes} to specify which account
+  to target.
+
+      iex> Goth.Token.for_scope({"myaccount@project.iam.gserviceaccount.com", "https://www.googleapis.com/auth/pubsub"})
+      {:ok, %Goth.Token{token: "23984723",
+                        type: "Bearer",
+                        scope: "https://www.googleapis.com/auth/pubsub",
+                        expires: 1453653825,
+                        account: "myaccount@project.iam.gserviceaccount.com"}}
 
   For using the token on subsequent requests to the Google API, just concatenate
   the `type` and `token` to create the authorization header. An example using
@@ -31,14 +43,15 @@ defmodule Goth.Token do
   alias Goth.Client
 
   @type t :: %__MODULE__{
-                    token: String.t,
-                    type:  String.t,
-                    scope: String.t,
-                    sub:   String.t | nil,
-                    expires: non_neg_integer
-                  }
+          token: String.t(),
+          type: String.t(),
+          scope: String.t(),
+          sub: String.t() | nil,
+          expires: non_neg_integer,
+          account: String.t()
+        }
 
-  defstruct [:token, :type, :scope, :sub, :expires]
+  defstruct [:token, :type, :scope, :sub, :expires, :account]
 
   @doc """
   Get a `%Goth.Token{}` for a particular `scope`. `scope` can be a single
@@ -51,10 +64,20 @@ defmodule Goth.Token do
       iex> Token.for_scope("https://www.googleapis.com/auth/pubsub")
       {:ok, %Goth.Token{expires: ..., token: "...", type: "..."} }
   """
-  @spec for_scope(scope :: String.t, sub :: String.t | nil) :: {:ok, t}
-  def for_scope(scope, sub \\ nil) do
-    case TokenStore.find(scope, sub) do
-      :error       -> retrieve_and_store!(scope, sub)
+  @spec for_scope(scope :: String.t(), sub :: String.t() | nil) :: {:ok, t}
+  def for_scope(info, sub \\ nil)
+
+  def for_scope(scope, sub) when is_binary(scope) do
+    case TokenStore.find({:default, scope}, sub) do
+      :error -> retrieve_and_store!({:default, scope}, sub)
+      {:ok, token} -> {:ok, token}
+    end
+  end
+
+  @spec for_scope({info :: {String.t(), atom()} | atom()}, sub :: String.t() | nil) :: {:ok, t}
+  def for_scope({account, scope}, sub) do
+    case TokenStore.find({account, scope}, sub) do
+      :error -> retrieve_and_store!({account, scope}, sub)
       {:ok, token} -> {:ok, token}
     end
   end
@@ -62,15 +85,37 @@ defmodule Goth.Token do
   @doc """
   Parse a successful JSON response from Google's token API and extract a `%Goth.Token{}`
   """
-  @spec from_response_json(scope :: String.t, sub :: String.t | nil, json :: String.t) :: t
-  def from_response_json(scope, sub \\ nil, json) do
-    {:ok, attrs} = json |> Poison.decode
+  def from_response_json(scope, sub \\ nil, json)
+
+  @spec from_response_json(String.t(), String.t() | nil, String.t()) :: t
+  def from_response_json(scope, sub, json) when is_binary(scope) do
+    {:ok, attrs} = json |> Poison.decode()
+
     %__MODULE__{
-      token:   attrs["access_token"],
-      type:    attrs["token_type"],
-      scope:   scope,
-      sub:     sub,
-      expires: :os.system_time(:seconds) + attrs["expires_in"]
+      token: attrs["access_token"],
+      type: attrs["token_type"],
+      scope: scope,
+      sub: sub,
+      expires: :os.system_time(:seconds) + attrs["expires_in"],
+      account: :default
+    }
+  end
+
+  @spec from_response_json(
+          {atom() | String.t(), String.t()},
+          String.t() | nil,
+          String.t()
+        ) :: t
+  def from_response_json({account, scope}, sub, json) do
+    {:ok, attrs} = json |> Poison.decode()
+
+    %__MODULE__{
+      token: attrs["access_token"],
+      type: attrs["token_type"],
+      scope: scope,
+      sub: sub,
+      expires: :os.system_time(:seconds) + attrs["expires_in"],
+      account: account
     }
   end
 
@@ -79,26 +124,30 @@ defmodule Goth.Token do
   although `Goth` automatically handles refreshing tokens for you, so you should
   rarely if ever actually need to call this method manually.
   """
-  @spec refresh!(t | String.t) :: {:ok, t}
-  def refresh!(%__MODULE__{scope: scope, sub: sub}), do: refresh!(scope, sub)
-  def refresh!(%__MODULE__{scope: scope}), do: refresh!(scope)
-  def refresh!(scope, sub \\ nil), do: retrieve_and_store!(scope, sub)
+  @spec refresh!(t | String.t()) :: {:ok, t}
+  def refresh!(%__MODULE__{account: account, scope: scope, sub: sub}),
+    do: refresh!({account, scope}, sub)
 
-  def queue_for_refresh(%__MODULE__{}=token) do
+  def refresh!(%__MODULE__{account: account, scope: scope}), do: refresh!({account, scope})
+
+  def refresh!({account, scope}, sub \\ nil), do: retrieve_and_store!({account, scope}, sub)
+
+  def queue_for_refresh(%__MODULE__{} = token) do
     diff = token.expires - :os.system_time(:seconds)
+
     if diff < 10 do
       # just do it immediately
-      Task.async fn ->
+      Task.async(fn ->
         __MODULE__.refresh!(token)
-      end
+      end)
     else
-      :timer.apply_after((diff-10)*1000, __MODULE__, :refresh!, [token])
+      :timer.apply_after((diff - 10) * 1000, __MODULE__, :refresh!, [token])
     end
   end
 
-  defp retrieve_and_store!(scope, sub) do
-    {:ok, token} = Client.get_access_token(scope, sub: sub)
-    TokenStore.store(scope, sub, token)
+  defp retrieve_and_store!({account, scope}, sub) do
+    {:ok, token} = Client.get_access_token({account, scope}, sub: sub)
+    TokenStore.store({account, scope}, sub, token)
     {:ok, token}
   end
 end
--- test/goth/token_store_test.exs
+++ test/goth/token_store_test.exs
@@ -4,21 +4,46 @@ defmodule Goth.TokenStoreTest do
   alias Goth.Token
 
   setup do
-    bypass = Bypass.open
+    bypass = Bypass.open()
     Application.put_env(:goth, :endpoint, "http://localhost:#{bypass.port}")
     Application.put_env(:goth, :token_source, :oauth)
     {:ok, bypass: bypass}
   end
 
   test "we can store an access token" do
-    TokenStore.store("devstorage.readonly, prediction", %Token{token: "123", type: "Bearer", expires: :os.system_time(:seconds)+1000})
+    TokenStore.store("devstorage.readonly, prediction", %Token{
+      token: "123",
+      type: "Bearer",
+      expires: :os.system_time(:seconds) + 1000
+    })
+
     {:ok, token} = TokenStore.find("devstorage.readonly, prediction")
     assert %Token{token: "123", type: "Bearer"} = token
     assert token.expires > :os.system_time(:seconds) + 900
   end
 
+  test "we can store an acces token for a specific account" do
+    TokenStore.store({"account@example.com", "devstorage.readonly, prediction"}, %Token{
+      token: "123",
+      type: "Bearer",
+      expires: :os.system_time(:seconds) + 1000,
+      account: "account@example.com"
+    })
+
+    {:ok, token} = TokenStore.find({"account@example.com", "devstorage.readonly, prediction"})
+    assert %Token{token: "123", type: "Bearer"} = token
+    assert token.expires > :os.system_time(:seconds) + 900
+  end
+
   test "a token is queued for refresh when stored" do
-    token = %Token{scope: "will-be-stale", token: "stale", type: "Bearer", sub: "sub@example.com", expires: :os.system_time(:seconds)+1000}
+    token = %Token{
+      scope: "will-be-stale",
+      token: "stale",
+      type: "Bearer",
+      sub: "sub@example.com",
+      expires: :os.system_time(:seconds) + 1000,
+      account: :default
+    }
 
     # if queued for later, we'll get back a reference
     {:ok, {_id, ref}} = TokenStore.store(token)
@@ -26,13 +51,28 @@ defmodule Goth.TokenStoreTest do
   end
 
   test "an expired token is refreshed immediately", %{bypass: bypass} do
-    Bypass.expect bypass, fn conn ->
-      Plug.Conn.resp(conn, 201, Poison.encode!(%{"access_token" => "fresh", "token_type" => "Bearer", "expires_in" => 3600}))
-    end
+    Bypass.expect(bypass, fn conn ->
+      Plug.Conn.resp(
+        conn,
+        201,
+        Poison.encode!(%{
+          "access_token" => "fresh",
+          "token_type" => "Bearer",
+          "expires_in" => 3600
+        })
+      )
+    end)
 
-    token = %Token{scope: "refresh-me", token: "stale", type: "Bearer", expires: 1}
+    token = %Token{
+      scope: "refresh-me",
+      token: "stale",
+      type: "Bearer",
+      expires: 1,
+      account: :default
+    }
+
     task = TokenStore.store(token)
-    ref  = Process.monitor(task.pid)
+    ref = Process.monitor(task.pid)
     assert_receive {:DOWN, ^ref, :process, _, :normal}, 1000
     assert {:ok, %Token{token: "fresh"}} = TokenStore.find("refresh-me")
   end
@@ -41,13 +81,20 @@ defmodule Goth.TokenStoreTest do
   # which go to sleep, it is not always happeneing
   test "find never returns stale tokens", %{bypass: _bypass} do
     token = %Token{scope: "expired", token: "stale", type: "Bearer", expires: 1}
-    {:ok, _pid} = GenServer.start_link(Goth.TokenStore,%{"expired" => token})
+    {:ok, _pid} = GenServer.start_link(Goth.TokenStore, %{"expired" => token})
     assert :error = TokenStore.find("expired")
   end
 
   test "token can be stored with sub" do
     sub = "sub@example.com"
-    TokenStore.store("devstorage.readonly, prediction", %Token{token: "123", type: "Bearer", sub: sub, expires: :os.system_time(:seconds)+1000})
+
+    TokenStore.store("devstorage.readonly, prediction", %Token{
+      token: "123",
+      type: "Bearer",
+      sub: sub,
+      expires: :os.system_time(:seconds) + 1000
+    })
+
     {:ok, token} = TokenStore.find("devstorage.readonly, prediction", sub)
     assert %Token{token: "123", type: "Bearer", sub: _} = token
     assert token.expires > :os.system_time(:seconds) + 900
@@ -57,11 +104,54 @@ defmodule Goth.TokenStoreTest do
     scopes = "devstorage.readonly, prediction, drive.readonly"
     sub1 = "sub1@example.com"
     sub2 = "sub2@example.com"
-    TokenStore.store(scopes, %Token{token: "123", type: "Bearer", sub: sub1, expires: :os.system_time(:seconds)+1000})
-    TokenStore.store(scopes, %Token{token: "123", type: "Bearer", sub: sub2, expires: :os.system_time(:seconds)+1000})
+
+    TokenStore.store(scopes, %Token{
+      token: "123",
+      type: "Bearer",
+      sub: sub1,
+      expires: :os.system_time(:seconds) + 1000,
+      account: :default
+    })
+
+    TokenStore.store(scopes, %Token{
+      token: "123",
+      type: "Bearer",
+      sub: sub2,
+      expires: :os.system_time(:seconds) + 1000,
+      account: :default
+    })
+
     assert :error == TokenStore.find(scopes)
     {:ok, token1} = TokenStore.find(scopes, sub1)
     {:ok, token2} = TokenStore.find(scopes, sub2)
+    assert token1 != token2
+  end
+
+  test "tokens with same scopes and subs but with different accounts are stored separately" do
+    scopes = "helloworld"
+    sub = "person@example.com"
+    account1 = "account1@example.com"
+    account2 = "account2@example.com"
+
+    TokenStore.store({account1, scopes}, %Token{
+      token: "123",
+      type: "Bearer",
+      sub: sub,
+      expires: :os.system_time(:seconds) + 1000,
+      account: account1
+    })
+
+    TokenStore.store({account2, scopes}, %Token{
+      token: "123",
+      type: "Bearer",
+      sub: sub,
+      expires: :os.system_time(:seconds) + 1000,
+      account: account2
+    })
+
+    assert :error == TokenStore.find(scopes, sub)
+    {:ok, %Token{account: ^account1} = token1} = TokenStore.find({account1, scopes}, sub)
+    {:ok, %Token{account: ^account2} = token2} = TokenStore.find({account2, scopes}, sub)
     assert token1 != token2
   end
 end
