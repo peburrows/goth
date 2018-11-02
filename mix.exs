94fcb6dedbc246da122576e429a47a53f373a210
Phil Burrows <github@philburrows.com>
2018-11-02 09:09:31 -0400

    version bump 0.11.0 --> 0.11.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Goth.Mixfile do
   def project do
     [
       app: :goth,
-      version: "0.11.0",
+      version: "0.11.1",
       description: description(),
       package: package(),
       elixirc_paths: elixirc_paths(Mix.env()),
