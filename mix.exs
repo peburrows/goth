b6e666e246d607280c63b1d47a9d0e2b0ed01e33
Phil Burrows <github@philburrows.com>
2019-06-19 21:33:49 -0500

    version bump 1.0.1 --> 1.1.0
    changes behavior of Goth.Token.for_scope such that it no longer raises a
    MatchError when a token can't be retrieved from the API, but instead
    returns {:error, any()} tuple

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Goth.Mixfile do
   def project do
     [
       app: :goth,
-      version: "1.0.1",
+      version: "1.1.0",
       description: description(),
       package: package(),
       elixirc_paths: elixirc_paths(Mix.env()),
