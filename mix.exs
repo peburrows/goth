f43f5c81245c7feb211686ab948681f80cefcad8
Wojtek Mach <wojtekmach@users.noreply.github.com>
2021-01-27 12:22:51 +0100

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Goth.Mixfile do
   def project do
     [
       app: :goth,
-      version: "1.2.0",
+      version: "1.3.0-dev",
       description: description(),
       package: package(),
       elixirc_paths: elixirc_paths(Mix.env()),
